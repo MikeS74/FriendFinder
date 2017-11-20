21740b62985bdecb9ad4ea4b7f5e3fa1c92593a2
Michael Sanchez <futurefreq@hotmail.com>
2017-11-20 13:03:13 -0800

    survey answers converting to integers

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendData = [
+    {
+name: "Austin",
+photo: "https://scontent-lax3-2.xx.fbcdn.net/v/t31.0-8/13581946_10210724093805617_7194933783292849585_o.jpg?oh=95612050c2ebe89b97af812724c0e53a&oe=5A93E819",
+scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+];
+
+module.exports = friendData;
